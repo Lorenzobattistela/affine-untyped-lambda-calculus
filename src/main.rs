8874f97c40db1799aa19e3237f8e06063a98fe62
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-02-26 16:52:32 -0300

    test normalization and parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-mod parser;
-
-use parser::Parser;
+use affine_lambda_calc::normalize;
+use affine_lambda_calc::Parser;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input: Vec<char> = "(@x.x @y.y)".chars().collect();
+    let input: Vec<char> = "(@x.x y)".chars().collect();
     let mut parser = Parser::new(input);
     let res = parser.parse();
     if let Some(term) = res {
         term.print();
+        let normalized = normalize(term);
+        println!("Normalized term: ");
+        normalized.print();
     } else {
         println!("Failed to parse input.");
     }
